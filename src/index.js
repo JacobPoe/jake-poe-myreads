31d2cdc4807ac14f3add93a5e920fa4a5b206663
Jake Poe <jacobpoe@gmail.com>
2023-09-05 16:57:42 -0400

    Fix trailing comma, run prettier

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
